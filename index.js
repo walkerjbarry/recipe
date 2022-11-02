e36d93d80592ba0c1609dd1f55350923cac6d709
barry_walker <barry.walker@esc13.txed.net>
2022-11-02 10:59:49 -0500

    Code cleanup

--- index.js
+++ index.js
@@ -2,25 +2,11 @@
 Create a Recipe App by integrating the "TheMealDB" API's random recipe endpoint.  When the page first loads, it should display a random recipe,
  complete with a picture of the meal, name of the meal, a button to watch the YouTube video, and the instructions of how to make the meal, along with the list of ingredients.
 -When a user clicks a button called "new recipe", it should show a new random and update the content on the page using JavaScript.
-
-    Components:
-    -------------
-
-    <h1 class="header">Let's Cook!</h1>
-    <p id="title"></p>
-    <img id="photo"/>
-    <p id="ingredients"></p>
-    <p id="instructions"></p>
-    <button id="video">How-To Video</button>
-    <button id="newRandom" onclick="loadRandom()">New Recipe</button>
-
-    - will load function onload, and will load the same function when the button is pushed.
 */ 
 function loadRandom() {
     const title = document.getElementById("title");
     const photo = document.getElementById("photo");
     const instructions = document.getElementById("instructions");
-    //const videoBox = document.getElementById("videoBox");
     const ingredientList = document.getElementById("ingredientList");
     const videoBox = document.getElementById("videoBox");
 
@@ -31,11 +17,11 @@ function loadRandom() {
             title.innerText = meal.strMeal;/*...such as strMeal, strInstructions, etc.*/
             photo.src = meal.strMealThumb;
             instructions.innerText = meal.strInstructions;  
-            const splitUrl = meal.strYoutube.split("=");
-            const videoId = splitUrl[1];
-            videoBox.src = "https://www.youtube.com/embed/" + videoId;
+            const splitUrl = meal.strYoutube.split("="); /*Here we use the .split method to separate the meal.strYoutube string by =  */
+            const videoId = splitUrl[1];                /*which (in this case) isolates the unique identifier for each video link*/
+            videoBox.src = "https://www.youtube.com/embed/" + videoId;  /*we then use concatenation to join the strings to create the value of the new src attribute for the <iframe> element*/
 
-            for (let i = 1; i <= 20; i++) { //again, we know there will only be max 20 ingredients in any recipe
+            for (let i = 1; i <= 20; i++) { /*again, we know there will only be max 20 ingredients in any recipe*/
                
                 const ingredientIndex = meal[`strIngredient${i}`];
                 if (ingredientIndex != "") { /*if the result of evaluating for keys entitled `strIngredient${i}`  *IS NOT an empty string, we run the following code*/
